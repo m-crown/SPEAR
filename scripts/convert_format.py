3a3ad9f56817a90540fb15dd8056d48a92bcb514
m-crown <30960941+m-crown@users.noreply.github.com>
2021-12-22 01:35:29 +0000

    expanding SPEAR fields in summary to include new scores

--- scripts/convert_format.py
+++ scripts/convert_format.py
@@ -12,7 +12,7 @@ import csv
 def main():
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('output_dir', metavar='spear_vcfs/', type=str,
-        help='Destination dir for summary csv files')
+        help='Destination dir for summary tsv files')
     parser.add_argument('--vcfs', nargs='+', required=True,
         help='Input VCF files')
     args = parser.parse_args()
@@ -20,22 +20,26 @@ def main():
     Path(args.output_dir).mkdir(parents=True, exist_ok=True)
     
     sample_summaries = []
+
+    #["sample_id", "POS", "REF", "ALT", "gene_name", "HGVS.nt", "consequence_type", "HGVS", "description", "RefSeq_acc", "residues","region", "domain", "contact_type", "NAb", "barns_class", "bloom_ace2", "VDS", "serum_escape", "mab_escape", "cm_mab_escape", "retained_escape", "ab_escape_fraction"] 
+    #write these col headers to a summary file then also write the single sample csvs to this summary following creation
+
     for vcf in args.vcfs:
         sample_name = Path(vcf).stem
         header, vcf , infocols = parse_vcf(vcf, split_info_cols = True)
         if len(vcf) != 0: #do not add summary if the vcf file is empty.
             vcf["SPEAR"] = vcf["SPEAR"].str.split(",", expand = False)
             vcf = vcf.explode("SPEAR")
-            vcf[["gene_name", "HGVS.c", "Annotation", "variant", "product", "protein_id", "residues","region", "domain", "contact_type", "NAb", "barns_class", "bloom_ace2", "VDS", "serum_escape", "mab_escape", "cm_mab_escape"]] = vcf["SPEAR"].str.split("|", expand = True)
+            vcf[["gene_name", "HGVS.c", "Annotation", "variant", "product", "protein_id", "residues","region", "domain", "contact_type", "NAb", "barns_class", "bloom_ace2", "VDS", "serum_escape", "mab_escape", "cm_mab_escape", "BEC_EF", "BEC_RES"]] = vcf["SPEAR"].str.split("|", expand = True)
             vcf.loc[vcf["gene_name"].str.contains('-'), "gene_name"] = "Intergenic_" + vcf.loc[vcf["gene_name"].str.contains('-'), "gene_name"]
             vcf.loc[vcf["Annotation"] == "synonymous_variant", "variant"] = vcf.loc[vcf["Annotation"] == "synonymous_variant", "HGVS.c"]
             per_sample_output = vcf.copy()
             per_sample_output["sample_id"] = sample_name
-            cols = ["sample_id", "POS", "REF", "ALT", "gene_name", "HGVS.c", "Annotation", "variant", "product", "protein_id", "residues","region", "domain", "contact_type", "NAb", "barns_class", "bloom_ace2", "VDS", "serum_escape", "mab_escape", "cm_mab_escape"]
-            per_sample_output[["region", "domain", "contact_type", "NAb", "barns_class", "bloom_ace2", "VDS", "serum_escape", "mab_escape", "cm_mab_escape"]] = per_sample_output[["region", "domain", "contact_type", "NAb", "barns_class", "bloom_ace2", "VDS", "serum_escape", "mab_escape", "cm_mab_escape"]].replace("[^-0-9a-zA-Z]+[~]+", "", regex = True)
-            per_sample_output[["residues","region", "domain", "contact_type", "NAb", "barns_class", "bloom_ace2", "VDS", "serum_escape", "mab_escape", "cm_mab_escape"]] = per_sample_output[["residues","region", "domain", "contact_type", "NAb", "barns_class", "bloom_ace2", "VDS", "serum_escape", "mab_escape", "cm_mab_escape"]].replace("~",",", regex = True)
+            cols = ["sample_id", "POS", "REF", "ALT", "gene_name", "HGVS.c", "Annotation", "variant", "product", "protein_id", "residues","region", "domain", "contact_type", "NAb", "barns_class", "bloom_ace2", "VDS", "serum_escape", "mab_escape", "cm_mab_escape", "BEC_EF", "BEC_RES"]
+            per_sample_output[["region", "domain", "contact_type", "NAb", "barns_class", "bloom_ace2", "VDS", "serum_escape", "mab_escape", "cm_mab_escape", "retained_escape", "ab_escape_fraction"]] = per_sample_output[["region", "domain", "contact_type", "NAb", "barns_class", "bloom_ace2", "VDS", "serum_escape", "mab_escape", "cm_mab_escape", "BEC_EF", "BEC_RES"]].replace("[^-0-9a-zA-Z]+[~]+", "", regex = True)
+            per_sample_output[["residues","region", "domain", "contact_type", "NAb", "barns_class", "bloom_ace2", "VDS", "serum_escape", "mab_escape", "cm_mab_escape", "retained_escape", "ab_escape_fraction"]] = per_sample_output[["residues","region", "domain", "contact_type", "NAb", "barns_class", "bloom_ace2", "VDS", "serum_escape", "mab_escape", "cm_mab_escape", "BEC_EF", "BEC_RES"]].replace("~",",", regex = True)
             per_sample_output = per_sample_output[cols]
-            per_sample_output.columns = ["sample_id", "POS", "REF", "ALT", "gene_name", "HGVS.nt", "consequence_type", "HGVS", "description", "RefSeq_acc", "residues","region", "domain", "contact_type", "NAb", "barns_class", "bloom_ace2", "VDS", "serum_escape", "mab_escape", "cm_mab_escape"] 
+            per_sample_output.columns = ["sample_id", "POS", "REF", "ALT", "gene_name", "HGVS.nt", "consequence_type", "HGVS", "description", "RefSeq_acc", "residues","region", "domain", "contact_type", "NAb", "barns_class", "bloom_ace2", "VDS", "serum_escape", "mab_escape", "cm_mab_escape", "BEC_EF", "BEC_RES"] 
             per_sample_output.to_csv(f'{args.output_dir}/{sample_name}.summary.tsv', sep = '\t', header = True, index = False)
             vcf["variant"] = vcf["Annotation"] + "_" + vcf["variant"]
             new_df = vcf[["gene_name", "variant"]].copy()
@@ -49,7 +53,7 @@ def main():
             sample_summaries.append(sample_summary)
         else:
             #touch file if empty  
-            Path(f'{args.output_dir}/{sample_name}.summary.tsv').touch()
+            Path(f'{args.output_dir}/summary/{sample_name}.summary.tsv').touch()
     sample_summaries.sort()
     with open(f'{args.output_dir}/summary.csv', "a") as fp:
         wr = csv.writer(fp, delimiter=',')
