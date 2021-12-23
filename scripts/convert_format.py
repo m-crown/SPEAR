e225a72a283877132f70253af5f494d55c1c81aa
m-crown <30960941+m-crown@users.noreply.github.com>
2021-12-23 15:57:25 +0000

    added contact counts to score summary

--- scripts/convert_format.py
+++ scripts/convert_format.py
@@ -34,7 +34,7 @@ def main():
         tsv_output = csv.writer(f_output, delimiter='\t')
         tsv_output.writerow(sample_summary_cols)
     
-    scores_columns = ["sample_id","total_variants","contact_type_variants", "region_variants", "region_variants", "ACE2_contact_variants", "trimer_contact_variants", "barns_class_variants", "bloom_ACE2" , "VDS", "serum_escape", "mAb_escape_all_classes", "class_1_mAb_escape", "class_2_mAb_escape", "class_3_mAb_escape", "class_4_mAb_escape", "BEC_sample_EF", "BEC_REF"]
+    scores_columns = ["sample_id","total_variants","contact_type_variants", "region_variants", "domain_variants", "ACE2_contact_counts","ACE2_contact_score","trimer_contact_counts", "trimer_contact_score", "barns_class_variants", "bloom_ACE2" , "VDS", "serum_escape", "mAb_escape_all_classes", "class_1_mAb_escape", "class_2_mAb_escape", "class_3_mAb_escape", "class_4_mAb_escape", "BEC_sample_EF", "BEC_REF"]
     with open(f'{args.output_dir}/spear_score_summary.tsv', 'w') as f_output:
         tsv_output = csv.writer(f_output, delimiter='\t')
         tsv_output.writerow(scores_columns)
@@ -66,29 +66,79 @@ def main():
             region_string = df_counts_to_string(region_counts)
             domain_counts = per_sample_output["domain"].str.split(",").explode().replace(r'^\s*$', np.nan, regex=True).value_counts()
             domain_string = df_counts_to_string(domain_counts)
-            contacts_df = per_sample_output["contact_type"].str.split(" ").explode().replace("", np.nan).str.split(":", expand = True).reset_index(drop = True)
-            contacts_df.rename(columns={0: "contact", 1: "contact_type"}, inplace = True)
-            contacts_df["contact_type"] = contacts_df["contact_type"].str.split("+")
-            contacts_df = contacts_df.explode(["contact_type"])
-            contacts_df["contact_type"] = contacts_df["contact_type"].str.extract(r'^([a-z-A-Z]+)_*', expand = False)
-            contacts_df["score"] = contacts_df["contact_type"].replace({"h-bond": 2, "contact": 1, "salt-bridge": 3})
-            contacts_scores = contacts_df.groupby(["contact"]).sum()
-            ace2_contacts_score = contacts_scores.loc[contacts_scores.index=="ACE2", "score"].values[0] if len(contacts_scores.loc[contacts_scores.index=="ACE2", "score"].values) == 1 else ""
-            trimer_contacts_score = contacts_scores.loc[contacts_scores.index=="trimer", "score"].values[0] if len(contacts_scores.loc[contacts_scores.index=="trimer", "score"].values) == 1 else ""
-            barns_counts = per_sample_output["barns_class"].explode(",").str.split("+").explode().replace(r'^\s*$', np.nan, regex=True).value_counts()
-            barns_counts.index = "class_" + barns_counts.index
-            barns_string = df_counts_to_string(barns_counts)
-            bloom_ace2_sum = per_sample_output["bloom_ace2"].str.split(",").explode().replace(r'^\s*$', np.nan, regex=True).astype("float").sum()
-            vds_sum = per_sample_output["VDS"].str.split(",").explode().replace(r'^\s*$', np.nan, regex=True).astype("float").sum()
-            serum_escape_sum = per_sample_output["serum_escape"].str.split(",").explode().replace(r'^\s*$', np.nan, regex=True).astype("float").sum()
-            mab_escape_all_sum = per_sample_output["mAb_escape"].str.split(",").explode().replace(r'^\s*$', np.nan, regex=True).astype("float").sum()
-            mab_escape_class_1_sum = per_sample_output["mAb_escape_class_1"].str.split(",").explode().replace(r'^\s*$', np.nan, regex=True).astype("float").sum()
-            mab_escape_class_2_sum = per_sample_output["mAb_escape_class_2"].str.split(",").explode().replace(r'^\s*$', np.nan, regex=True).astype("float").sum()
-            mab_escape_class_3_sum = per_sample_output["mAb_escape_class_3"].str.split(",").explode().replace(r'^\s*$', np.nan, regex=True).astype("float").sum()
-            mab_escape_class_4_sum = per_sample_output["mAb_escape_class_4"].str.split(",").explode().replace(r'^\s*$', np.nan, regex=True).astype("float").sum()
-            bec_sample_ef_score = per_sample_output["BEC_sample_EF"].str.split(",").explode().replace(r'^\s*$', np.nan, regex=True).astype("float").mean()
-            bec_ref_score = per_sample_output["BEC_RES"].str.split(",").explode().replace(r'^\s*$', np.nan, regex=True).astype("float").sum()
-            scores_list = [sample_name,sample_variant_number, type_string, region_string, domain_string, ace2_contacts_score, trimer_contacts_score, barns_string,bloom_ace2_sum , vds_sum, serum_escape_sum, mab_escape_all_sum, mab_escape_class_1_sum, mab_escape_class_2_sum, mab_escape_class_3_sum, mab_escape_class_4_sum, bec_sample_ef_score, bec_ref_score]
+            if per_sample_output["contact_type"].isin([""]).all():
+                ace2_contacts_score = ""
+                trimer_contacts_score = ""
+            else:
+                contacts_df = per_sample_output["contact_type"].str.split(" ").explode().replace("", np.nan).str.split(":", expand = True).reset_index(drop = True)
+                contacts_df.rename(columns={0: "contact", 1: "contact_type"}, inplace = True)
+                contacts_df["contact_type"] = contacts_df["contact_type"].str.split("+")
+                contacts_df = contacts_df.explode(["contact_type"])
+                contacts_df["contact_type"] = contacts_df["contact_type"].str.extract(r'^([a-z-A-Z]+)_*', expand = False)
+                contacts_df["score"] = contacts_df["contact_type"].replace({"h-bond": 2, "contact": 1, "salt-bridge": 3})
+                contacts_scores = contacts_df.groupby(["contact"]).sum()
+                ace2_contacts_score = contacts_scores.loc[contacts_scores.index=="ACE2", "score"].values[0] if len(contacts_scores.loc[contacts_scores.index=="ACE2", "score"].values) == 1 else ""
+                trimer_contacts_score = contacts_scores.loc[contacts_scores.index=="trimer", "score"].values[0] if len(contacts_scores.loc[contacts_scores.index=="trimer", "score"].values) == 1 else ""
+                contacts_counts = contacts_df.groupby(["contact"]).count()
+                ace2_contacts_sum = contacts_counts.loc[contacts_scores.index=="ACE2", "contact_type"].values[0] if len(contacts_counts.loc[contacts_scores.index=="ACE2", "contact_type"].values) == 1 else ""
+                trimer_contacts_sum = contacts_counts.loc[contacts_scores.index=="trimer", "contact_type"].values[0] if len(contacts_counts.loc[contacts_scores.index=="trimer", "contact_type"].values) == 1 else ""
+            if per_sample_output["barns_class"].isin([""]).all():
+                barns_string = ""
+            else:
+                barns_counts = per_sample_output["barns_class"].explode(",").str.split("+").explode().replace(r'^\s*$', np.nan, regex=True).value_counts()
+                barns_counts.index = "class_" + barns_counts.index
+                barns_string = df_counts_to_string(barns_counts)
+            
+            if per_sample_output["bloom_ace2"].isin([""]).all():
+                bloom_ace2_sum = per_sample_output["bloom_ace2"].str.split(",").explode().replace(r'^\s*$', np.nan, regex=True).astype("float").sum()
+            else:
+                bloom_ace2_sum = ""
+            
+            if per_sample_output["VDS"].isin([""]).all():
+                vds_sum = per_sample_output["VDS"].str.split(",").explode().replace(r'^\s*$', np.nan, regex=True).astype("float").sum()
+            else:
+                vds_sum = ""
+            
+            if per_sample_output["serum_escape"].isin([""]).all():
+                serum_escape_sum = ""
+            else:
+                serum_escape_sum = per_sample_output["serum_escape"].str.split(",").explode().replace(r'^\s*$', np.nan, regex=True).astype("float").sum()
+            
+            if per_sample_output["mAb_escape"].isin([""]).all():
+                mab_escape_all_sum = ""
+            else:
+                mab_escape_all_sum = per_sample_output["mAb_escape"].str.split(",").explode().replace(r'^\s*$', np.nan, regex=True).astype("float").sum()
+            
+            if per_sample_output["mAb_escape_class_1"].isin([""]).all():
+                mab_escape_class_1_sum = ""
+            else:
+                mab_escape_class_1_sum = per_sample_output["mAb_escape_class_1"].str.split(",").explode().replace(r'^\s*$', np.nan, regex=True).astype("float").sum()
+            
+            if per_sample_output["mAb_escape_class_2"].isin([""]).all():
+                mab_escape_class_2_sum = ""
+            else:
+                mab_escape_class_2_sum = per_sample_output["mAb_escape_class_2"].str.split(",").explode().replace(r'^\s*$', np.nan, regex=True).astype("float").sum()
+            
+            if per_sample_output["mAb_escape_class_3"].isin([""]).all():
+                mab_escape_class_3_sum = ""
+            else:    
+                mab_escape_class_3_sum = per_sample_output["mAb_escape_class_3"].str.split(",").explode().replace(r'^\s*$', np.nan, regex=True).astype("float").sum()
+            
+            if per_sample_output["mAb_escape_class_4"].isin([""]).all():
+                mab_escape_class_4_sum = ""
+            else:
+                mab_escape_class_4_sum = per_sample_output["mAb_escape_class_4"].str.split(",").explode().replace(r'^\s*$', np.nan, regex=True).astype("float").sum()
+            
+            if per_sample_output["BEC_sample_EF"].isin([""]).all():
+                bec_sample_ef_score = ""
+            else:
+                bec_sample_ef_score = per_sample_output["BEC_sample_EF"].str.split(",").explode().replace(r'^\s*$', np.nan, regex=True).astype("float").mean()
+            
+            if per_sample_output["BEC_RES"].isin([""]).all():
+                bec_res_score = ""
+            else:
+                bec_res_score = per_sample_output["BEC_RES"].str.split(",").explode().replace(r'^\s*$', np.nan, regex=True).astype("float").sum()
+            scores_list = [sample_name,sample_variant_number, type_string, region_string, domain_string,ace2_contacts_sum, ace2_contacts_score, trimer_contacts_sum,trimer_contacts_score, barns_string,bloom_ace2_sum , vds_sum, serum_escape_sum, mab_escape_all_sum, mab_escape_class_1_sum, mab_escape_class_2_sum, mab_escape_class_3_sum, mab_escape_class_4_sum, bec_sample_ef_score, bec_res_score]
             scores_df = pd.DataFrame([scores_list], columns=scores_columns)
             scores_df.to_csv(f'{args.output_dir}/spear_score_summary.tsv', sep = '\t', mode='a', header=False, index = False)
 
@@ -104,7 +154,7 @@ def main():
             sample_summary = [sample_name, sample_variants]
             sample_summaries.append(sample_summary)
         else:
-            Path(f'{args.output_dir}/per_sample_annotation/{sample_name}.summary.tsv').touch() #touch file if empty  
+            Path(f'{args.output_dir}/per_sample_annotation/{sample_name}.annotation.tsv').touch() #touch file if empty  
     sample_summaries.sort() #alphabetically ordered samples 
     with open(f'{args.output_dir}/spear_variant_summary.csv', "a") as fp:
         wr = csv.writer(fp, delimiter=',')
