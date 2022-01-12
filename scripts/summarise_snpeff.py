e0b431f233acfcd5d4e4aff07b0071c1bfb52f70
m-crown <30960941+m-crown@users.noreply.github.com>
2022-01-12 21:21:06 +0000

    added new method to handle multi-AA substitutions

--- scripts/summarise_snpeff.py
+++ scripts/summarise_snpeff.py
@@ -11,7 +11,7 @@ import numpy as np
 import re
 from shutil import copy
 
-def convert_snpeff_annotation(vcf, gb_mapping, locus_tag_mapping):
+def convert_snpeff_annotation(vcf, gb_mapping, locus_tag_mapping, respos_df):
 
   def filter_spear_summary(summaries):
     regexp = re.compile(r'ORF1(a|ab) polyprotein')
@@ -20,6 +20,50 @@ def convert_snpeff_annotation(vcf, gb_mapping, locus_tag_mapping):
     else:
       summaries = [summary for summary in summaries if not regexp.search(summary)]
     return summaries
+
+  def get_ref_res(refres_info, respos, gene):
+    if respos == respos:
+        return(refres_info.loc[refres_info.index == int(respos), gene].values[0])
+    else:
+        return np.nan
+
+  def populate_alt_res(variant, pos, respos):
+    #extract the column residue position from HGVS.p
+    #this is only required for variants matching delins, del, missense or synoynmous variants (e.g. not frameshift, stop or insertion - these are handled directly.)
+    pos = int(pos)
+    if re.match('p\.[A-Z][0-9]+_[A-Z][0-9]+del$', variant):
+        max_res = int(re.match('p\.[A-Z][0-9]+_[A-Z]([0-9]+)+del$', variant)[1])
+        #only give a deletion to positions which are within the deletion range
+        if respos <= max_res:
+            alt_res = "del" 
+        else:
+            alt_res = ""
+    elif re.match('p\.[A-Z]+[0-9]+[A-Z\*]+|fs', variant):
+        alt_residues = re.match('p\.[A-Z]+[0-9]+([A-Z\*]+|fs)', variant)[1]
+        if alt_residues != "fs":
+            alt_res_list = [res for res in alt_residues]
+        else:
+            alt_res = "fs"
+        #try to get the alt_res from list and if position is out of range leave alt res empty (i.e. doesnt exist)
+        try:
+            alt_res = alt_res_list[pos]
+        except:
+            alt_res = ""           
+    elif re.match('p\.[A-Z][0-9]+_[A-Z][0-9]+delins[A-Z\*]+?',variant):
+        max_res = int(re.match('p\.[A-Z][0-9]+_[A-Z]([0-9]+)delins[A-Z\*]+', variant)[1])
+        alt_residues = re.match('p\.[A-Z][0-9]+_[A-Z][0-9]+delins([A-Z\*]+)', variant)[1]
+        alt_res_list = [res for res in alt_residues]
+        if respos <= max_res:
+            try:
+                alt_res = alt_res_list[pos]
+            except:
+                alt_res = "del"
+        else:
+            alt_res = ""
+    else:
+        alt_res = ""      
+    return alt_res
+
 
   #takes a input a dataframe row, splits the ann field into a new
   vcf["ANN2"] = vcf["ANN"].str.split(',') #put the split ANN field into a new column to preserve original snpeff value
@@ -34,7 +78,7 @@ def convert_snpeff_annotation(vcf, gb_mapping, locus_tag_mapping):
   vcf["product"] = vcf["Feature_ID"].map(lambda x: gb_mapping.get(x))
   vcf.loc[vcf["product"].isna(), "product"] = vcf.loc[vcf["product"].isna(), "Gene_Name"]
   vcf["protein_id"] = vcf["product"].map(lambda x: locus_tag_mapping.get(x))
-  vcf["protein_id"] = vcf["protein_id"].fillna("")
+  vcf["protein_id"] = vcf["protein_id"]
 
   #convert HGVS annotation to per residue for residue annotation. 
   #handle deletions and insertions residues first
@@ -42,43 +86,56 @@ def convert_snpeff_annotation(vcf, gb_mapping, locus_tag_mapping):
   vcf["end_pos"] = vcf["end_pos"].fillna(vcf["start_pos"])
   vcf[["start_pos", "end_pos"]] = vcf[["start_pos", "end_pos"]].fillna(0).astype("int")
   vcf["ins_length"] = vcf['ins'].str.len().fillna(0).astype('int')
-  range_df = pd.DataFrame(list(range(i, j+1)) for i, j in vcf[["start_pos", "end_pos"]].values).add_prefix("position_")
-  vcf["length"] = range_df.count(axis = 1)
-  range_df = "del" + range_df.fillna(0).astype('int').astype('str')
-  range_df = range_df.replace("del0", "")
-  cols = range_df.columns
-  vcf = pd.concat([vcf,range_df], axis = 1)
-  vcf["inserted_residues"] = vcf["start_res"] + vcf["start_pos"].astype('str') + vcf["ins"].fillna('').astype('str')
-  vcf["range"] = vcf.apply(lambda x : list(range(x['ins_length'],x['length'])),1)
-  vcf.loc[(vcf["Annotation"].astype("str").isin(["missense_variant","synonymous_variant"]) == False) & (vcf["change"] == "delins"), "residues"] = vcf.loc[vcf["change"] == "delins","inserted_residues"] + "~" + vcf.apply(lambda x: '~'.join(x[b] for b in cols[x.range]),axis=1)
-  vcf.loc[(vcf["Annotation"].astype("str").isin(["missense_variant","synonymous_variant"]) == False) & (vcf["change"] == "del"), "residues"] = vcf.loc[vcf["change"] == "del"].apply(lambda x: '~'.join(x[b] for b in cols[x.range]),axis=1)
-  vcf.loc[(vcf["Annotation"].astype("str").isin(["missense_variant","synonymous_variant"]) == False) & (vcf["change"] == "ins"), "residues"] = vcf.loc[vcf["change"] == "ins", "start_res"] + vcf["start_pos"].astype("str") + "-" + vcf.loc[vcf["change"] == "ins","ins"] + "-" + vcf.loc[vcf["change"] == "ins","end_res"] + vcf.loc[vcf["change"] == "ins","end_pos"].astype('str')
-  #frameshift and stop variants
-  vcf.loc[vcf["Annotation"].astype("str") == "frameshift_variant", "residues"] = vcf.loc[vcf["Annotation"].astype("str") == "frameshift_variant", "variant"].str.replace("^p\.", '')
-  vcf.loc[vcf["Annotation"].astype("str") == "stop_gained", "residues"] = vcf.loc[vcf["Annotation"].astype("str") == "stop_gained", "variant"].str.extract("^p\.([A-Z][0-9]+)\*") + "stop"
-  #now handle missense variants and synonymous variants
-  vcf[["start_res2", "start_pos2", "end_res2"]] = vcf["variant"].str.extract('([A-Z]+)([0-9]+)([A-Z]+)')
-  vcf["end_pos2"] = vcf["end_pos"].fillna(0).astype("int")
-  vcf["start_res2"] = vcf["start_res2"].fillna("").astype("str")
-  vcf["end_res2"] = vcf["end_res2"].fillna("").astype("str")
-  vcf["start_pos2"] = vcf["start_pos2"].fillna(0).astype("int")
-  vcf.loc[vcf["Annotation"].astype("str").isin(["missense_variant","synonymous_variant"]), "end_pos2"] = vcf.loc[vcf["Annotation"].astype("str").isin(["missense_variant","synonymous_variant"]), "start_pos2"].fillna(0).astype("int") + vcf.loc[vcf["Annotation"].astype("str").isin(["missense_variant","synonymous_variant"]), "start_res2"].str.len() -1 
-  vcf.loc[vcf["Annotation"].astype("str").isin(["missense_variant","synonymous_variant"]), "start_pos2"] = np.array([list(range(i, j+1)) for i, j in vcf.loc[vcf["Annotation"].astype("str").isin(["missense_variant","synonymous_variant"]),["start_pos2", "end_pos2"]].values],dtype = object)
-  vcf.loc[vcf["Annotation"].astype("str").isin(["missense_variant","synonymous_variant"]), "start_res2"] = np.array([list(x) for x in vcf.loc[vcf["Annotation"].astype("str").isin(["missense_variant","synonymous_variant"]), "start_res2"]], dtype = object)
-  vcf.loc[vcf["Annotation"].astype("str").isin(["missense_variant","synonymous_variant"]), "end_res2"] = np.array([list(x) for x in vcf.loc[vcf["Annotation"].astype("str").isin(["missense_variant","synonymous_variant"]), "end_res2"]], dtype = object)
-  split_residues = vcf.loc[vcf["Annotation"].astype("str").isin(["missense_variant","synonymous_variant"]),["start_res2", "start_pos2", "end_res2"]].values.tolist()
-  residues = []
-  for item in split_residues:
-    #allows iteration over lists to zip together, single sample 
-    item[0] = item[0] if type(item[0]) is list else [item[0]]
-    item[1] = item[1] if type(item[1]) is list else [item[1]]
-    item[2] = item[2] if type(item[2]) is list else [item[2]]
-    residue = '~'.join(list(map(''.join,list(x for x in zip(item[0], [str(i) for i in item[1]], item[2])))))
-    residues.append(residue)
-  vcf.loc[vcf["Annotation"].astype("str").isin(["missense_variant","synonymous_variant"]), "residues"] = residues
-  vcf["residues"] = vcf["residues"].fillna("")
-  cols = list(cols) + ["start_res", "start_pos", "end_res", "end_pos", "change", "ins", "ins_length", "length","inserted_residues","range", "start_res2", "start_pos2", "end_res2", "end_pos2"]
+
+  #Because multiple substitutions is incorrectly handled range_df wont necessarily expand to correct size, so need to set the minimum size of the dataframe to be the size of these multiple insertions, then expand to del/delins
+  maxmultiaa = vcf.loc[vcf.variant.str.contains(r'p\.[A-Z]+[0-9]+[A-Z\*]{2,}') == True, "variant"].str.extract(r'p\.[A-Z]+[0-9]+([A-Z\*]{2,})', expand = False).str.len().max()
+  range_df= pd.DataFrame(list(range(i, j+1)) for i, j in vcf[["start_pos", "end_pos"]].values)
+  additional_cols = [x for x in list(range(0, maxmultiaa)) if x not in range_df.columns.tolist()]
+  range_df[additional_cols] = np.nan
+  range_df = range_df.add_prefix("respos_")
+  
+  range_cols = range_df.columns.tolist()
+  all_cols = []
+  refres_cols = []
+  altres_cols = []
+  final_res_cols = []
+  for respos in range_cols:
+      refres_col = "ref_res" + re.match('respos(_[0-9]+)', respos)[1]
+      altres_col = "alt_res" + re.match('respos(_[0-9]+)', respos)[1]
+      final_res_col = "residue" + re.match('respos(_[0-9]+)', respos)[1]
+      all_cols.append(refres_col)
+      refres_cols.append(refres_col)
+      altres_cols.append(altres_col)
+      all_cols.append(respos)
+      all_cols.append(altres_col)
+      all_cols.append(final_res_col)
+      final_res_cols.append(final_res_col)
+  range_df[[refres_cols, altres_cols, final_res_cols]] = np.nan
+  range_df = range_df[all_cols]
+  range_df[["variant", "product"]] = vcf[["variant", "product"]] 
+  residue_col_list = list(range(1, range_df.shape[1] - 2, 4))
+
+  range_df.loc[range_df.variant.str.contains(r'p\.[A-Z]+[0-9]+[A-Z\*]+') == True, "respos_0"] = range_df.loc[range_df.variant.str.contains(r'p\.[A-Z]+[0-9]+[A-Z\*]+') == True, "variant"].str.extract(r'p\.[A-Z]+([0-9]+)[A-Z\*]+')[0]
+  
+  for x in range(1,len(residue_col_list)):
+      range_df.iloc[:, residue_col_list[x]] = range_df.iloc[:, residue_col_list[x-1]].fillna(-2).astype(int) + 1
+
+  for ref, pos, alt, final in zip(refres_cols, range_cols, altres_cols, final_res_cols):
+      col_pos = re.search('([0-9]+)', pos)[1]
+      range_df.loc[range_df["variant"].str.match(r'^p\.'), ref] = range_df.loc[range_df["variant"].str.match(r'^p\.')].apply(lambda x: get_ref_res(respos_df , x[pos], x["product"]), axis=1)
+      range_df.loc[range_df["variant"].str.match(r'^p\.'), alt] = range_df.loc[range_df["variant"].str.match(r'^p\.')].apply(lambda x: populate_alt_res(x["variant"], col_pos, x[pos]), axis=1).fillna("")
+      range_df.loc[range_df[alt].isin(["", np.nan]) == False , final] = range_df.loc[range_df[alt].isin(["", np.nan]) == False, ref].astype("str") + range_df.loc[range_df[alt].isin(["", np.nan]) == False , pos].astype("str") + range_df.loc[range_df[alt].isin(["", np.nan]) == False ,alt].astype("str")
+  
+  range_df["residues"] = np.nan
+  range_df = range_df.fillna("")
+  range_df.loc[range_df["variant"].str.contains('p\.[A-Z][0-9]+_[A-Z][0-9]+ins[A-Z]+$', regex = True), "residues"] = range_df.loc[range_df["variant"].str.contains('p\.[A-Z][0-9]+_[A-Z][0-9]+ins[A-Z]+$', regex = True), "variant"].str.extract(r'p\.([A-Z][0-9]+)_[A-Z][0-9]+ins[A-Z]+$', expand = False).astype("str") + "-" + range_df.loc[range_df["variant"].str.contains('p\.[A-Z][0-9]+_[A-Z][0-9]+ins[A-Z]+$', regex = True), "variant"].str.extract(r'p\.[A-Z][0-9]+_[A-Z][0-9]+ins([A-Z]+)$', expand = False).astype("str") + "-" + range_df.loc[range_df["variant"].str.contains('p\.[A-Z][0-9]+_[A-Z][0-9]+ins[A-Z]+$', regex = True), "variant"].str.extract(r'p\.[A-Z][0-9]+_([A-Z][0-9]+)ins[A-Z]+$', expand = False).astype("str")
+  range_df.loc[range_df["variant"].str.contains('p\.[A-Z][0-9]+_[A-Z][0-9]+ins[A-Z]+$', regex = True) == False, "residues"] = range_df[final_res_cols].agg('~'.join, axis=1)
+  range_df.loc[range_df["variant"].str.contains('p\.[A-Z][0-9]+fs$', regex = True), "residues"] = range_df.loc[range_df["variant"].str.contains('p\.[A-Z][0-9]+fs$', regex = True), "variant"].str.extract(r'p\.([A-Z][0-9]+fs)$', expand = False).astype("str")
+  range_df["residues"] = range_df["residues"].str.replace('~+$', '', regex = True)
+  vcf = pd.concat([vcf,range_df["residues"]], axis = 1)
+  cols = ["start_res", "start_pos", "end_res", "end_pos", "change", "ins", "ins_length"]
   vcf.drop(cols, axis = 1, inplace = True)
+
   #annotate residue specific information for each variant.
   cols = ["Gene_Name", "HGVS.c", "Annotation", "variant", "product", "protein_id", "residues"]
   vcf["SUM"] = vcf[cols].apply(lambda row: '|'.join(row.values.astype(str)), axis=1)
@@ -136,6 +193,7 @@ def main():
       help ='Data files for peptide subpositions')
   args = parser.parse_args()
 
+  respos_df = pd.read_pickle(f'{args.data_dir}/respos.pkl')
   header, vcf, infocols = parse_vcf(args.vcf)
   if not args.allow_seq_end:
     vcf = vcf[vcf["POS"].between(56, 29804)].reset_index(drop = True)
@@ -155,7 +213,7 @@ def main():
         else:
           genbank_mapping[feature.qualifiers["locus_tag"][0]] = feature.qualifiers["product"][0]
           locus_tag_mapping[feature.qualifiers["product"][0]] = feature.qualifiers["protein_id"][0]
-    df = convert_snpeff_annotation(df.copy(), genbank_mapping, locus_tag_mapping)
+    df = convert_snpeff_annotation(df.copy(), genbank_mapping, locus_tag_mapping, respos_df)
     infocols.append("SUM")
     df.loc[df["ANN"] == "", "ANN"] = "no_annotation"
     for col in infocols:
