ab53d9ee5eca811dbab31a79b6a79a2f5cb522d8
m-crown <30960941+m-crown@users.noreply.github.com>
2022-04-01 15:25:09 +0100

    update version

--- scripts/summary_report.py
+++ scripts/summary_report.py
@@ -1642,7 +1642,7 @@ def main():
                             <p> Generated On: ''' + report_date + '''</p>
                         </div>
                         <div class="col-4 text-center">
-                            <p> SPEAR Version 0.8.1 </p>
+                            <p> SPEAR Version 0.8.2 </p>
                         </div>
                         <div class="col-4 text-right">
                             <p></p>
