844110e8105cf2d1422bd8fa67268ade65d8789f
m-crown <30960941+m-crown@users.noreply.github.com>
2022-04-21 11:48:46 +0100

    update pango script to work on all systems

--- scripts/summary_report.py
+++ scripts/summary_report.py
@@ -1814,7 +1814,7 @@ def main():
                             <p> Generated On: ''' + report_date + '''</p>
                         </div>
                         <div class="col-4 text-center">
-                            <p> SPEAR Version 1.0.0 </p>
+                            <p> SPEAR Version 1.0.2 </p>
                         </div>
                         <div class="col-4 text-right">
                             <p></p>
